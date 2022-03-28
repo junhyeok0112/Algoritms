5d81943261cc25f3ca93360c791d8b4f93677d5c
junhyeok0112 <wd78wg@naver.com>
2022-03-29 08:39:54 +0900

    [3/29]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,245 +1,83 @@
-package com.company;
-
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
-
-class Employee{
-    String name =" hit";
-    public String toString(){return name;}
-}
-
-class PartEmployee extends Employee{
-    void work(){
-        System.out.println("w2");
-    }
-}
-
-public class Main {
-
-    public static void main(String[] args) {
-        Set<String> s = new TreeSet<>();
-        s.add("서울");
-        s.add("구미");
-        s.add("광주");
-        s.add("대전");
-        s.add("부울경");
-        s.add("서울");
-        System.out.println(s);
-       }
-
-
-}
-
-
-class Pair implements Comparable<Pair>{
-    int num;
-    ArrayList<Integer> arr ;
-
-    Pair(int num , ArrayList<Integer> arr){
-        this.num = num;
-        this.arr = arr;
-
-    }
-    @Override
-    public int compareTo(Pair o) {
-        if(this.num > o.num){
-            return 1;
-        } else if(this.num < o.num){
-            return -1;
-        } else{
-            if(this.arr.get(0) > o.arr.get(0)){
-                return -1;
-            } else if(this.arr.get(0) < o.arr.get(0)){
-                return 1;
-            } else{
-                if(this.arr.get(1) > o.arr.get(1)){
-                    return -1;
-                } else if(this.arr.get(1) < o.arr.get(1)){
-                    return 1;
-                } else{
-                    if(this.arr.get(2) > o.arr.get(2)){
-                        return -1;
-                    } else if(this.arr.get(2) < o.arr.get(2)){
-                        return 1;
-                    } else{
-                        if(this.arr.get(3) > o.arr.get(3)){
-                            return -1;
-                        } else if(this.arr.get(3) < o.arr.get(3)){
-                            return 1;
-                        } else{
-                            if(this.arr.get(4) > o.arr.get(4)){
-                                return -1;
-                            } else if(this.arr.get(4) < o.arr.get(4)){
-                                return 1;
-                            } else{
-                                if(this.arr.get(5) > o.arr.get(5)){
-                                    return -1;
-                                } else if(this.arr.get(5) < o.arr.get(5)){
-                                    return 1;
-                                } else{
-                                    if(this.arr.get(6) > o.arr.get(6)){
-                                        return -1;
-                                    } else if(this.arr.get(6) < o.arr.get(6)){
-                                        return 1;
-                                    } else{
-                                        if(this.arr.get(7) > o.arr.get(7)){
-                                            return -1;
-                                        } else if(this.arr.get(7) < o.arr.get(7)){
-                                            return 1;
-                                        } else{
-                                            if(this.arr.get(8) > o.arr.get(8)){
-                                                return -1;
-                                            } else if(this.arr.get(8) < o.arr.get(8)){
-                                                return 1;
-                                            } else{
-                                                if(this.arr.get(9) > o.arr.get(9)){
-                                                    return -1;
-                                                } else if(this.arr.get(9) < o.arr.get(9)){
-                                                    return 1;
-                                                } else{
-                                                    if(this.arr.get(10) > o.arr.get(10)){
-                                                        return -1;
-                                                    } else{
-                                                        return 1;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-class Solution {
-
-
-    public static int[] solution(int n, int[] info) {
-        int[] answer = {};
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        PriorityQueue<Pair> res = new PriorityQueue<>();
-
-        for (int a = n; a >= 0; a--) {      //a 부터 0점
-            for (int b = n - a; b >= 0; b--) {
-                for (int c = n - a - b; c >= 0; c--) {
-                    for (int d = n - a - b - c; d >= 0; d--) {
-                        for (int e = n - a - b - c - d; e >= 0; e--) {
-                            for (int f = n - a - b - c - d - e; f >= 0; f--) {
-                                for (int g = n - a - b - c - d - e - f; g >= 0; g--) {
-                                    for (int h = n - a - b - c - d - e - f - g; h >= 0; h--) {
-                                        for (int i = n - a - b - c - d - e - f - g - h; i >= 0; i--) {
-                                            for (int j = n - a - b - c - d - e - f - g - h - i; j >= 0; j--) {
-                                                for (int k = n - a - b - c - d - e - f - g - h - i - j; k >= 0; k--) {
-                                                    int lion = 0, apeach = 0;
-                                                    if (a + b + c + d + e + f + g + h + i + j + k != n)
-                                                        continue;
-                                                    if (info[0] >= k && info[0] != 0) {
-                                                        apeach += 10;
-                                                    } else {
-                                                        if (k != 0)
-                                                            lion += 10;
-                                                    }
-                                                    if (info[1] >= j && info[1] != 0) {
-                                                        apeach += 9;
-                                                    } else {
-                                                        if (j != 0)
-                                                            lion += 9;
-                                                    }
-                                                    if (info[2] >= i && info[2] != 0) {
-                                                        apeach += 8;
-                                                    } else {
-                                                        if (i != 0)
-                                                            lion += 8;
-                                                    }
-                                                    if (info[3] >= h && info[3] != 0) {
-                                                        apeach += 7;
-                                                    } else {
-                                                        if (h != 0)
-                                                            lion += 7;
-                                                    }
-                                                    if (info[4] >= g && info[4] != 0) {
-                                                        apeach += 6;
-                                                    } else {
-                                                        if (g != 0)
-                                                            lion += 6;
-                                                    }
-                                                    if (info[5] >= f && info[5] != 0) {
-                                                        apeach += 5;
-                                                    } else {
-                                                        if (f != 0)
-                                                            lion += 5;
-                                                    }
-                                                    if (info[6] >= e && info[6] != 0) {
-                                                        apeach += 4;
-                                                    } else {
-                                                        if (e != 0)
-                                                            lion += 4;
-                                                    }
-                                                    if (info[7] >= d && info[7] != 0) {
-                                                        apeach += 3;
-                                                    } else {
-                                                        if (d != 0)
-                                                            lion += 3;
-                                                    }
-                                                    if (info[8] >= c && info[8] != 0) {
-                                                        apeach += 2;
-                                                    } else {
-                                                        if (c != 0)
-                                                            lion += 2;
-                                                    }
-                                                    if (info[9] >= b && info[9] != 0) {
-                                                        apeach += 1;
-                                                    } else {
-                                                        if (b != 0)
-                                                            lion += 1;
-                                                    }
-                                                    if (lion > apeach) {
-                                                        ArrayList<Integer> tmp = new ArrayList<>();
-                                                        tmp.add(a);
-                                                        tmp.add(b);
-                                                        tmp.add(c);
-                                                        tmp.add(d);
-                                                        tmp.add(e);
-                                                        tmp.add(f);
-                                                        tmp.add(g);
-                                                        tmp.add(h);
-                                                        tmp.add(i);
-                                                        tmp.add(j);
-                                                        tmp.add(k);
-                                                        res.add(new Pair(lion , tmp));
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if (res.isEmpty())
-            ans.add(-1);
-        else {
-            ArrayList<Integer> v = res.poll().arr;
-            for (int i = 10; i >= 0; i--) {
-                ans.add(v.get(i));
-            }
-        }
-
-        int ansSize = ans.size();
-        answer = new int[ansSize];
-        for(int i= 0 ; i<ansSize; i++){
-            answer[i] = ans.get(i);
-        }
-        return answer;
-    }
-}
+//package com.company;
+//
+//import java.util.*;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//
+//
+//public class Main {
+//    static String[] sheet = {"team_name" , ":" ,"","application_name" ,":","","error_level" ,":" ,"","message" ,":" ,""};
+//    static String str=  "team_name : recommend application_name : recommend error_level : info message : RecommendSucces11";
+//    public int main(String[] args) {
+//        int answer = 0;
+//        //하나의 String[i]에 한셋트의 로그가 들어옴. 즉 로그마다 보면됨
+//        String[] logs = new String[1];
+//        logs[0] = str;
+//        int len = logs.length;
+//        for(int i = 0 ; i<len; i++){
+//            if(logs[i].length() >100){ //길이 100 넘어갈 경우
+//                answer++;
+//                continue;
+//            }
+//            //앞 뒤에 공백이 있을경우
+//            if(logs[i].charAt(0) ==' ' || logs[i].charAt(logs[i].length()-1) == ' '){
+//                answer++;
+//                continue;
+//            }
+//            //공백이 연속으로 있을 경우
+//            boolean chk = false;
+//            for(int t = 0 ;t <logs[i].length()-1; t++){
+//                if(logs[i].charAt(t) == ' ' && logs[i].charAt(t+1) == ' '){ //공백연속이면 체크
+//                    answer++;
+//                    chk = true;
+//                    break;
+//                }
+//            }
+//            if(chk) continue;   //공백 체크하고 나왔으면 넘김
+//
+//            String[] temp = logs[i].split(" ");
+//            if(temp.length != 12){
+//                answer++;
+//                continue;
+//            }
+//            //제대로된 형식인지 확인
+//
+//            for(int t = 0 ; t<temp.length;t++){
+//                System.out.print(temp[t] + " ");
+//            }
+//            boolean chk2 = false;
+//            for(int j=  0 ; j <12 ;j++){
+//                System.out.println(i);
+//                if(j == 2 ||j ==5 || j==8 || j==11){    //에러메세지들
+//                    //알파벳소문자 ,대문자로만
+//                    System.out.println(j);
+//                    for(int t=0; t<temp[j].length(); t++) {
+//                        if (String.valueOf(temp[j].charAt(t)).matches("[^a-zA-Z0-9\\s]")) { //특수문자 인 경우
+//                            answer++;
+//                            chk2= true;
+//                            System.out.println(i+"특");
+//                            break;
+//                        }
+//                        else { //특수문자가 아닌 경우
+//                            if(Character.isDigit(temp[j].charAt(t))) { //숫자인 경우
+//                                answer++;
+//                                chk2 = true;
+//                                System.out.println(i + " 숫");
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }else{
+//                    if(!temp[i].equals(sheet[i])){
+//                        answer++;
+//                        break;
+//                    }
+//                }
+//                if(chk2) break;
+//            }
+//        }
+//        return answer;
+//    }
+//}
+//
+//
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -2,74 +2,19 @@
 //
 //import java.util.*;
 //
-//class Now{
-//    int value;
-//    int block;      //몇번 블록인지
-//    Now(int value , int block){
-//        this.value = value;
-//        this.block = block;
-//    }
-//}
-//
 //class Solution {
-//    public String[] solution(String[] code) {
-//        String[] answer = {};
-//        ArrayList<String> ans = new ArrayList<>();
-//        ArrayList<Stack<Now>> box = new ArrayList<>();
-//        Stack<Integer> block = new Stack<>();
-//        for(int i = 0 ; i<26 ; i++){
-//            box.add(new Stack<Now>());
-//        }
-//        //System.out.println(countChar(code[6], '.'));
-//        for(int i = 0 ; i<code.length; i++){
-//            String s = code[i];
-//            int cnt = countChar(s , '.');       //점의 개수
-//            if(block.isEmpty()) block.add(cnt); //비어있으면 넣기
-//            else{                               //비어 있지않아
-//                if(block.peek() == cnt)  continue; //같으면 이전 블록과같음
-//                else if(block.peek() < cnt) block.push(cnt);   //포함되는 경우
-//                else{
-//                    while(!block.isEmpty() && block.peek() >cnt){                   //같은 block 나올떄까지 pop
-//                        block.pop();
-//                    }
-//                    for(Stack<Now> sta : box){                  //이전 블락에 있던 변수도 모두 제거
-//                        while(!sta.isEmpty() && sta.peek().block >cnt){                   //같은 block 나올떄까지 pop
-//                            sta.pop();
-//                        }
-//                    }
-//                }
-//            }
-//            //block 계산 끝 -> 이제 명령어 처리
-//            s = s.substring(cnt , s.length());        //명령어만 남기고 처리
-//            if(s.contains("print")){
-//                char valName = s.charAt(s.length()-1);    //출력할 변수명
-//                if(box.get(valName - 'a').isEmpty()){   //만약 비어있으면
-//                    ans.add("error");
-//                    continue;
-//                }
-//                int val = box.get(valName - 'a').peek().value;   //변수값 변수명에 해당하는 변수의 가장 나중 값
-//                ans.add(valName+"="+val);
-//            }else{                                      //변수 생성 명령어면
-//                char valName = s.charAt(0);
-//                int val = Integer.parseInt(s.substring(2, s.length()));
-//                box.get(valName - 'a').push(new Now(val,i));    //값과 몇번째 block인지
-//            }
-//        }
-//        int ansSize = ans.size();
-//        answer = new String[ansSize];
-//        for(int i = 0 ; i<ansSize;i++){
-//            answer[i] = ans.get(i);
-//        }
+//    static String[] sheet = {"team_name", ":", "", "application_name", ":", "", "error_level", ":", "", "message", ":", ""};
+//    static String str = "team_name : recommend application_name : recommend error_level : info message : RecommendSucces11";
 //
-//        return answer;
+//    public void solution(String[] code) {
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        arr.add(1);
+//        arr.add(2);
+//        arr.set(1, 1);
+//        System.out.print(arr.get(1));
 //    }
-//
-//
-//    public int countChar(String str, char ch) {
-//        return str.length() - str.replace(String.valueOf(ch), "").length();
-//    }
-//
 //}
+//
 //
 //public static void main(String[] args){
 //
